5ac8531f595615a8d16139709e4e8b40cb39b325
Ralph Olarte <olarteralphanthony@yahoo.com>
2026-01-08 21:28:16 -0500

    HTTP Basics

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,3 +1,10 @@
-console.log('Express Tutorial')
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log("user hit the server");
+  res.end("home page");
+});
+
+server.listen(5000);
 
 // 3:58:12
