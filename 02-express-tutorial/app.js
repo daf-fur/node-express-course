adb9609658a8dfa9e2868f3a612d74988047ca99
Ralph Olarte <olarteralphanthony@yahoo.com>
2026-01-08 21:34:47 -0500

    Http - Headers

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,10 +1,11 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("user hit the server");
-  res.end("home page");
+  res.writeHead(200, { "content-type": "text/html" });
+  res.write("<h1>home page </h1>");
+  res.end(); // always needed to end communication
 });
 
 server.listen(5000);
 
-// 3:58:12
+// 4:20:04
