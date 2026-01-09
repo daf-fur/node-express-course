1ac4c8634e3f880764ec15fac5670e508765cf7f
Ralph Olarte <olarteralphanthony@yahoo.com>
2026-01-09 08:54:40 -0500

    Http - Request - URL

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,23 +1,29 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/html" });
-  res.write("<h1>home page </h1>");
-  res.end(); // always needed to end communication
+  // console.log(req.method); (default is GET)
+  const url = req.url;
+
+  // home page
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>home page </h1>");
+    res.end(); // always needed to end communication
+  }
+  // about page
+  else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>about page </h1>");
+    res.end();
+  }
+  // error (404)
+  else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>page not found</h1>");
+    res.end();
+  }
 });
 
 server.listen(5000);
 
 // 4:20:04
-
-// Status Codes
-
-// 100 - Information Response
-// 200 - Successful Response
-// 300 - Redirection
-// 400 - Client Error Response
-
-
-// Mime Types
-// type/subtype
-
