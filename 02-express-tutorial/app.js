50b48b30bf884b50b77454135e42f7b86c61b1eb
Ralph Olarte <olarteralphanthony@yahoo.com>
2026-01-12 16:48:16 -0500

    JSON Basics - Part1

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,25 +1,12 @@
 const express = require("express");
-const path = require("path");
-
 const app = express();
 
-// setup static and middleware
-app.use(express.static("./public")); // gets every resource in the public folder
-
-// app.get("/", (req, res) => {
-// home page
-// res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-// adding to static assets
-// SSR - service side rendering
-// });
-
-app.all("*", (req, res) => {
-  // if every code above does not get used, use this one (error)
-  res.status(404).send("resourse not found");
+app.get("/", (req, res) => {
+  res.json();
 });
 
 app.listen(5000, () => {
-  console.log("server is listening on port 5000....");
+  console.log("Server is listening on port 5000");
 });
 
-// 5:13:49
+// 5:28:04
