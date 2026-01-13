1fa5b88455d186d6ff34bb70420fc37d49710a7d
Ralph Olarte <olarteralphanthony@yahoo.com>
2026-01-13 13:14:54 -0500

    Route Parameters & Parameters Extra Info

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -3,11 +3,40 @@ const app = express();
 const { products } = require("./data.js");
 
 app.get("/", (req, res) => {
-  res.json([products]);
+  res.send('<h1> Home Page </h1> <a href="/api/products">products</a>');
+});
+
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
+});
+
+app.get("/api/products/:productID", (req, res) => {
+  /* console.log(req);
+  console.log(req.params); */
+  const { productID } = req.params; // productID is a string so it needs to be converted later
+
+  const singleProduct = products.find(
+    // .find iterates through the whole list of objects
+    (product) => product.id === Number(productID) // product represents one object at a time
+  );
+
+  if (!singleProduct) {
+    return res.status(404).send("Product Does Not Exist");
+  }
+  return res.json(singleProduct);
+});
+
+app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
+  console.log(req.params);
+  res.send("hello world");
 });
 
 app.listen(5000, () => {
   console.log("Server is listening on port 5000");
 });
 
-// 5:28:04
+// 5:50:44
