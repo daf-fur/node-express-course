f5c5d80c7bd242544609a8676d6d4cba88677ad3
Ralph Olarte <olarteralphanthony@yahoo.com>
2026-01-16 13:28:33 -0500

    .put and .delete, Use of Postman (testing)

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -23,12 +23,60 @@ app.post("/api/people", (req, res) => {
   res.status(201).json({ sucess: true, person: name });
 });
 
+app.post("/api/postman/people", (req, res) => {
+  const { name } = req.body;
+  console.log(req.body);
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "please provide name value" });
+  }
+  res.status(201).json({ sucess: true, data: [...people, name] });
+});
+
 app.post("/login", (req, res) => {
   const { name } = req.body;
   if (name) {
     return res.status(200).send(`Welcome ${name}`);
   }
   res.status(401).send("Please Provide Credentials");
+});
+
+app.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const person = people.find((person) => person.id === Number(id)); // looks for matching id
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${id}` }); // if id does not match
+  }
+  const newPeople = people.map((person) => {
+    // replaces the name of the corresponding id
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+  res.status(200).json({ success: true, data: newPeople });
+});
+
+app.delete("/api/people/:id", (req, res) => {
+  const person = people.find((person) => person.id === Number(req.params.id)); // looks for matching id
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${req.params.id}` }); // if id does not match
+  }
+  const newPeople = people.filter(
+    (
+      person // .filter means to keep the elements
+    ) => person.id !== Number(req.params.id)
+  ); // if it's not equal to the req.params.id given then keep it
+  return res.status(200).json({ success: true, data: newPeople }); 
 });
 
 app.listen(5000, () => {
